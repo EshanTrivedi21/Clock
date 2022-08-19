fd4547ab9e8884aa0deb68559f93b4664535f9c0
Eshan Trivedi <eshan.trivedi.9@gmail.com>
2022-08-19 17:23:19 +0530

    home page v1

--- static/analog.js
+++ static/analog.js
@@ -1,1 +1,29 @@
 "use-strict";
+
+setInterval(displayTimeAnalog);
+
+const hourHand = document.querySelector('[data-hour-hand]');
+const minuteHand = document.querySelector('[data-minute-hand]');
+const secondHand = document.querySelector('[data-second-hand]');
+
+function displayTimeAnalog() {
+
+    const dateTime =  new Date();
+
+    let seconds = dateTime.getSeconds() / 60;
+    let minutes = (seconds + dateTime.getMinutes()) / 60;
+    let hours =  (minutes + dateTime.getHours()) / 12;
+
+    setRotation(secondHand, seconds);
+
+    setRotation(minuteHand, minutes);
+
+    setRotation(hourHand, hours);
+
+}
+
+function setRotation (element, rotationRatio) {
+    element.style.setProperty("--rotation" , rotationRatio * 360);
+}
+
+displayTimeAnalog();
--- static/digital.js
+++ static/digital.js
@@ -1,1 +1,50 @@
 "use-strict";
+
+function displayTimeDigital() {
+
+    let dateTime =  new Date();
+
+    let day = dateTime.getDay();
+    let month = dateTime.getMonth();
+    let date = dateTime.getDate();
+    let year = dateTime.getFullYear();
+
+    let hours =  dateTime.getHours();
+    let minutes = dateTime.getMinutes();
+    let seconds = dateTime.getSeconds();
+
+    const session = document.getElementById("digital-clock--session");
+
+    const weekday = ["Sun","Mon","Tue","Wed","Thur","Fri","Sat"];
+    dayWeek = weekday[day];
+
+    const months = ["Jan","Feb","Mar","Apr","May","Jun","Jul","Aug","Sept","Oct","Nov","Dec"];
+    monthYear = months[month];
+
+    if ( hours >= 12 ) {
+        session.innerHTML = "Pm";
+        hours = hours - 12;
+    } else {
+        session.innerHTML = "Am";
+    };
+
+    if ( seconds <= 9 ) {
+        seconds = "0" + `${seconds}`;
+    } 
+
+    if ( minutes <= 9 ) {
+        minutes = "0" + `${minutes}`;
+    } 
+
+    if ( hours <= 9 ) {
+        hours = "0" + `${hours}`;
+    } 
+
+    document.getElementById("digital-clock--hours").innerHTML = hours;
+    document.getElementById("digital-clock--minutes").innerHTML = minutes;
+    document.getElementById("digital-clock--seconds").innerHTML = seconds;
+    document.getElementById("digital-clock--date").innerHTML = dayWeek + " , " + date + " " + monthYear + " " + year;
+
+}
+
+setInterval(displayTimeDigital);
