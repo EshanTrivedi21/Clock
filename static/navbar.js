1a657f5836685c3546389aab520bd96081e22f2a
Eshan Trivedi <eshan.trivedi.9@gmail.com>
2022-08-22 19:54:32 +0530

    navbar + home final

--- static/navbar.js
+++ static/navbar.js
@@ -1,2 +1,13 @@
 'use-strict';
 
+const hamburgir = document.getElementsByClassName("hamburger")[0];
+const navbarLinks = document.getElementsByClassName("navbar-links")[0];
+
+hamburgir.addEventListener ( 
+    'click' , function() {
+        navbarLinks.classList.toggle('active');
+        document.querySelector(".caption").classList.toggle('hidden');
+        document.querySelector(".hamburger").classList.toggle('hidden');
+        document.querySelector(".hamburger-cross").classList.toggle('hidden');      
+    }
+)
